4efbc4d56f828f5d64d0798056160eb7bc74f35e
huangsir <1817412295@qq.com>
2023-08-10 21:38:42 +0800

    订单取消、再来一单、历史订单查询代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,8 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kwh
@@ -17,4 +19,12 @@ public interface OrderDetailMapper {
      * @param orderDetailList
      */
     void insertBatch(ArrayList<OrderDetail> orderDetailList);
+
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
 }
