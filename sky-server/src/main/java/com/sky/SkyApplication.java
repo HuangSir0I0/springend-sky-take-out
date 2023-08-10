74598e02f4edd9fc4774fe7cf83f20d5f3ab87dd
huangsir <1817412295@qq.com>
2023-08-10 14:52:21 +0800

    地址簿代码开发

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -12,10 +12,19 @@ import org.springframework.util.DigestUtils;
 @Slf4j
 @EnableCaching//开启缓存注解
 public class SkyApplication {
+
+    /*
+     * 解决druid 日志报错：discard long time none received connection:xxx
+     * */
+    static {
+        System.setProperty("druid.mysql.usePingMethod","false");
+    }
+
     public static void main(String[] args) {
 //        String s = DigestUtils.md5DigestAsHex("123456".getBytes());
 //        log.info(s);
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
     }
+
 }
